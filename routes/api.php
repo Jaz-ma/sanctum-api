a21bc65f1945a5a64c88d7121bd8b4a94392437e
Jaz-ma <potatoorgy3@gmail.com>
2022-07-17 04:45:01 +0100

    register

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Models\Product; use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,10 +15,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::apiResource('products', ProductController::class);
+//Public Routes
+// Route::apiResource('products', ProductController::class);
+Route::get('products',[ProductController::class,'index']);
+Route::get('products/{product}',[ProductController::class,'show']);
 Route::get('products/search/{name}', [ProductController::class,'search']);
+Route::post('register',[AuthController::class,'register']);
 
+//Protected Routes
+Route::group(['middleware' => 'auth:sanctum'],function(){
+Route::post('products',[ProductController::class,'show']);
+Route::delete('products/{product}',[ProductController::class,'destroy']);
+Route::put('products/{product}',[ProductController::class,'update']);
+
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
